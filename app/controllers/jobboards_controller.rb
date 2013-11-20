194cac72866867d19a94092a557a6ccb3d48be9b
Larry Pilgrim <lpilgrim2@student.cvtc.edu>
2013-11-20 22:57:55 +0000

    Add email field

--- app/controllers/jobboards_controller.rb
+++ app/controllers/jobboards_controller.rb
@@ -69,6 +69,6 @@ class JobboardsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def jobboard_params
-      params.require(:jobboard).permit(:jobTitle, :company, :hours, :full, :partTime, :salary, :description, :requirements, :website)
+      params.require(:jobboard).permit(:jobTitle, :company, :hours, :full, :partTime, :salary, :description, :requirements, :website, :email)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131117043208) do
+ActiveRecord::Schema.define(version: 20131120223755) do
 
   create_table "jobboards", force: true do |t|
     t.text     "jobTitle"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20131117043208) do
     t.text     "website"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
