6c348e8fb8a7d2e5d4ed585a5fde891dece7513e
KaylieAnderson <kanderson182@student.cvtc.edu>
2013-11-22 22:34:01 +0000

    Uses @jobboards.order(:created_at).reverse.each to sort job's list

--- app/models/jobboard.rb
+++ app/models/jobboard.rb
@@ -1,4 +1,5 @@
 class Jobboard < ActiveRecord::Base
  
+  #validates so that no field can be left empty
   validates  :jobTitle,  :company,  :hours,  :full,  :partTime,  :salary,  :description,  :requirements,  :website,  :email,  presence: true
 end
