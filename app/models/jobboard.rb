d457d72615b16de3458fc8a752a56b49ea00462e
KaylieAnderson <kanderson182@student.cvtc.edu>
2013-11-22 21:14:36 +0000

    Completes validation story

--- app/models/jobboard.rb
+++ app/models/jobboard.rb
@@ -1,2 +1,4 @@
 class Jobboard < ActiveRecord::Base
+ 
+  validates  :jobTitle,  :company,  :hours,  :full,  :partTime,  :salary,  :description,  :requirements,  :website,  :email,  presence: true
 end
