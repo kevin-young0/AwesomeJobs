468e705f560beec077c2cb8377e4dcd1a0cdb93d
KaylieAnderson <kanderson182@student.cvtc.edu>
2013-12-02 05:31:48 +0000

    Implements radio buttons for approved field, validate radio buttons, edits website Edit links, adds Return to Admin Home links, Unhindes destroy on Adim Home page

--- app/models/jobboard.rb
+++ app/models/jobboard.rb
@@ -2,15 +2,13 @@ class Jobboard < ActiveRecord::Base
   before_create :set_approval_to_false
 
   #validates so that no field can be left empty
- validates  :jobTitle,  :company,  :hours,  :full,  :partTime,  :salary,  :description,  :requirements,  :website,  :email,  presence: true
+  validates  :jobTitle,  :company,  :hours,  :full,  :partTime,  :salary,  :description,  :requirements,  :website,  :email,  presence: true
+  validates_inclusion_of :approved, :in => [true, false]
 
- #setting approval to false in DB
+  #setting approval to false in DB
   def set_approval_to_false
     self.approved = false
     true
   end
-
- #approved droped down box (selet box) types
- APPROVE_TYPES = [ "Approve Job",  "Deny"]
   
 end
