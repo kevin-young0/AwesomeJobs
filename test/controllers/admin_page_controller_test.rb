179ac5f26283fb1c0c2401cb681be5d83e50f501
Larry Pilgrim <lpilgrim2@student.cvtc.edu>
2013-12-05 04:13:36 +0000

    Adds date column for admin page.

--- test/controllers/admin_page_controller_test.rb
+++ test/controllers/admin_page_controller_test.rb
@@ -4,7 +4,7 @@ class AdminPageControllerTest < ActionController::TestCase
   test "should get admin" do
     @request.env["HTTP_AUTHORIZATION"] = "Basic " + Base64::encode64("admin:secret")
    get :admin
-    assert_response :success
+     assert_response :success
     
  end
 
